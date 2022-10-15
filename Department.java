a80cc4369c96964a0cde656274085fb9a01f1e99
Ethan Boileau <eboileau@uiowa.edu>
2022-10-15 10:25:20 -0500

    department class update

--- Department.java
+++ Department.java
@@ -3,5 +3,28 @@ public class Department {
     private int numNurses;
     private int maxNurseCapacity;
 
+    public int getMaxNurseCapacity() {
+        return maxNurseCapacity;
+    }
 
+    public int getNumNurses() {
+        return numNurses;
+    }
+    public String getDeptName(){
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public void setMaxNurseCapacity(int maxNurseCapacity) {
+        this.maxNurseCapacity = maxNurseCapacity;
+    }
+
+    public void setNumNurses(int numNurses) {
+        this.numNurses = numNurses;
+    }
 }
+
+
